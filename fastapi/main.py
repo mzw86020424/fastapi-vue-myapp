de0c1094b4c5cb1455a952fdd494309d802b1537
mzw86020424 <mzw86020424@gmail.com>
2021-12-26 16:00:12 +0900

    ドキュメントタグ

--- fastapi/main.py
+++ fastapi/main.py
@@ -18,7 +18,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/user')
+@app.post('/user', tags=['users'])
 def create_user(request: User, db: Session = Depends(get_db)):
     new_user = models.User(
         name=request.name, 
@@ -29,14 +29,14 @@ def create_user(request: User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user/{id}', response_model=ShowUser)
+@app.get('/user/{id}', response_model=ShowUser, tags=['users'])
 def get_user(id: int, db: Session=Depends(get_db)):
     user = db.query(models.User).filter(models.User.id ==id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with the id {id} is not available')
     return user
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(blog: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -44,12 +44,12 @@ def create(blog: Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog', response_model=List[ShowBlog])
+@app.get('/blog', response_model=List[ShowBlog], tags=['blogs'])
 def all_fetch(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=ShowBlog, tags=['blogs'])
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -58,7 +58,7 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
         # return {'detail':f'Blog with the id {id} is not available'}
     return blog
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id: int, request: Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -68,7 +68,7 @@ def update(id: int, request: Blog, db: Session = Depends(get_db)):
     
     return 'Update compelted'
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def delete(id: int, db: Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
