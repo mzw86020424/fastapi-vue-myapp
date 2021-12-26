e3a012028ca282c0d5579a44d3dc38b29672289b
mzw86020424 <mzw86020424@gmail.com>
2021-12-26 11:04:35 +0900

    モデルを作成

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from .schemas import Blog
+from schemas import Blog
+from database import Base, engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.post('/blog')
 def create(blog: Blog):
