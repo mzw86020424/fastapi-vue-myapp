2992c9a509dd144d4b145a7f65700a20aa582f2e
mzw86020424 <mzw86020424@gmail.com>
2021-12-26 16:41:26 +0900

    Relationship（外部キーの設定）

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 from database import Base
 
 class User(Base):
@@ -8,6 +10,7 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+    blogs  = relationship('Blog', back_populates='creator')
 
 class Blog(Base):
     __tablename__ = "blogs"
@@ -15,3 +18,7 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    # Userクラスのidカラムを外部キーとして指定
+    user_id = Column(Integer, ForeignKey('users.id'))
+    # 特定のBlogを投稿したUser情報を受け取る
+    creator = relationship('User', back_populates='blogs')
