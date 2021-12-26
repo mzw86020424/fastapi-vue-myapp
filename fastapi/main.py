f242f8d1325fb87c07f901b984afd58412f53d3e
mzw86020424 <mzw86020424@gmail.com>
2021-12-26 14:58:17 +0900

    レスポンスモデル

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from schemas import Blog
+from schemas import Blog, ShowBlog
 import models
 from database import Base, engine, sessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 
@@ -24,12 +25,12 @@ def create(blog: Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[ShowBlog])
 def all_fetch(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=ShowBlog)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -43,7 +44,7 @@ def update(id: int, request: Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} is not found')
-    blog.update(request.dict())
+    blog.update(request)
     db.commit()
     
     return 'Update compelted'
--- fastapi/schemas.py
+++ fastapi/schemas.py
@@ -4,5 +4,12 @@ class Blog(BaseModel):
     title: str
     body: str
     
-    # class Config:
-    #     orm_mode = True
+    class Config:
+        orm_mode = True
+    
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    
+    class Config:
+        orm_mode = True
